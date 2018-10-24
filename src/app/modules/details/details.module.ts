0ee25f199428a09571f44cca3952869e6ec952e4
purusottamray07 <44378090+purusottamray07@users.noreply.github.com>
2018-10-24 22:53:48 +0530

    Created property details component

--- src/app/modules/details/details.module.ts
+++ src/app/modules/details/details.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DetailsRoutingModule } from './details-routing.module';
+import { PropertyDetailsComponent } from './property-details/property-details.component';
 
 @NgModule({
   imports: [
     CommonModule,
     DetailsRoutingModule
   ],
-  declarations: []
+  declarations: [PropertyDetailsComponent]
 })
 export class DetailsModule { }

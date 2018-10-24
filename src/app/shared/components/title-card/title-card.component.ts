4169451ba372ced2854214665e2ae606a58d6f2c
unknown <iamathreya@gmail.com>
2018-10-24 23:21:05 +0530

    Adding Input to TitleCardComponent and re-using it

--- src/app/shared/components/title-card/title-card.component.ts
+++ src/app/shared/components/title-card/title-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title-card.component.scss']
 })
 export class TitleCardComponent implements OnInit {
+
+  @Input() title;
 
   constructor() { }
 
